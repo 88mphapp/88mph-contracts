c9b03d7f079c3a0a9d8ac2a5dfa9f11e3f04d319
Zefram Lou <zeframlou@gmail.com>
2020-05-15 11:49:18 -0700

    Updated deployment scripts

--- scripts/deploy-compound-dai.js
+++ scripts/deploy-compound-dai.js
@@ -12,14 +12,27 @@ async function main() {
   const feeModel = await FeeModel.new();
   console.log(`Deployed FeeModel at address ${feeModel.address}`);
 
+  const NFT = env.artifacts.require("NFT");
+  const depositNFT = await NFT.new("88mph Compound-Pool Deposit", "88mph-Compound-Deposit");
+  console.log(`Deployed depositNFT at address ${depositNFT.address}`);
+  const fundingNFT = await NFT.new("88mph Compound-Pool Long Position", "88mph-Compound-Long");
+  console.log(`Deployed fundingNFT at address ${fundingNFT.address}`);
+
   const DInterest = env.artifacts.require("DInterest");
-  const UIRMultiplier = BigNumber(0.5 * 1e18).integerValue().toFixed(); // Minimum safe avg interest rate multiplier
+  const UIRMultiplier = BigNumber(0.75 * 1e18).integerValue().toFixed(); // Minimum safe avg interest rate multiplier
   const MinDepositPeriod = 90 * 24 * 60 * 60; // 90 days in seconds
-  const dInterestPool = await DInterest.new(UIRMultiplier, MinDepositPeriod, market.address, stablecoinAddress, feeModel.address);
+  const MaxDepositAmount = BigNumber(10000 * 1e18).toFixed(); // 10000 stablecoins
+  const dInterestPool = await DInterest.new(UIRMultiplier, MinDepositPeriod, MaxDepositAmount, market.address, stablecoinAddress, feeModel.address, depositNFT.address, fundingNFT.address);
   console.log(`Deployed DInterest at address ${dInterestPool.address}`);
 
   await market.transferOwnership(dInterestPool.address);
-  console.log(`Transferred CompoundERC20Market's ownership to ${dInterestPool.address}`);
+  console.log(`Transferred AaveMarket's ownership to ${dInterestPool.address}`);
+
+  await depositNFT.transferOwnership(dInterestPool.address);
+  console.log(`Transferred depositNFT's ownership to ${dInterestPool.address}`);
+
+  await fundingNFT.transferOwnership(dInterestPool.address);
+  console.log(`Transferred fundingNFT's ownership to ${dInterestPool.address}`);
 }
 
 main()

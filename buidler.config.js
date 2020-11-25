2a33de5e3492b2c92095a409e642d5c858e49c2c
Zefram Lou <zeframlou@gmail.com>
2020-11-24 17:38:07 -0800

    Update oneSplitAddress in deploy configs

--- buidler.config.js
+++ buidler.config.js
@@ -38,7 +38,7 @@ module.exports = {
         mnemonic: secret.mnemonic
       },
       gas: 'auto',
-      gasPrice: 42.0000001e9
+      gasPrice: 34.0000001e9
     },
     buidlerevm: {
       blockGasLimit: 9950000,

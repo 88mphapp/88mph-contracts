77f0a1775ccea79bfb033cbd2ad8bdb7edb4720a
Zefram Lou <zeframlou@gmail.com>
2021-04-01 20:27:47 -0700

    Add deployment script for LinearDecayInterestModel

---
+++ deploy/LinearDecayInterestModel.js
@@ -0,0 +1,20 @@
+const BigNumber = require('bignumber.js')
+
+module.exports = async ({ web3, getNamedAccounts, deployments, getChainId, artifacts }) => {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const poolConfig = require('../deploy-configs/get-pool-config')
+
+  const deployResult = await deploy('LinearDecayInterestModel', {
+    from: deployer,
+    args: [
+      BigNumber(poolConfig.multiplierIntercept).toFixed(),
+      BigNumber(poolConfig.multiplierSlope).toFixed()
+    ]
+  })
+  if (deployResult.newlyDeployed) {
+    log(`LinearDecayInterestModel deployed at ${deployResult.address}`)
+  }
+}
+module.exports.tags = ['LinearDecayInterestModel']
+module.exports.dependencies = []

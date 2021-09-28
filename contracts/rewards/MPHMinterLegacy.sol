08791f01f19d03fe88605b2d0523c24d747adf61
ZeframLou <zeframlou@gmail.com>
2021-09-28 00:40:59 -0700

    Fix MPHMinterLegacy

--- contracts/rewards/MPHMinterLegacy.sol
+++ contracts/rewards/MPHMinterLegacy.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.4;
 
+import {MPHToken} from "./MPHToken.sol";
 import {MPHMinter} from "./MPHMinter.sol";
 
 /**
     @title Dummy MPHMinter that doesn't mint anything. For legacy support.
 */
 contract MPHMinterLegacy {
+    bytes32 public constant WHITELISTED_POOL_ROLE =
+        keccak256("WHITELISTED_POOL_ROLE");
+
     MPHMinter public mphMinter;
 
-    constructor(address _mphMinter) {
-        mphMinter = MPHMinter(_mphMinter);
+    constructor(MPHMinter _mphMinter) {
+        mphMinter = _mphMinter;
     }
 
     function mintDepositorReward(
@@ -23,11 +27,23 @@ contract MPHMinterLegacy {
     }
 
     function takeBackDepositorReward(
-        address, /*from*/
-        uint256, /*mintMPHAmount*/
-        bool /*early*/
-    ) external pure returns (uint256) {
-        return 0;
+        address from,
+        uint256 mintMPHAmount,
+        bool early
+    ) external returns (uint256) {
+        require(
+            mphMinter.hasRole(WHITELISTED_POOL_ROLE, msg.sender),
+            "NOT_POOL"
+        );
+        if (!early) {
+            return 0;
+        }
+        mphMinter.mph().transferFrom(
+            from,
+            mphMinter.govTreasury(),
+            mintMPHAmount
+        );
+        return mintMPHAmount;
     }
 
     function mintFunderReward(
